7c44d1423b88230eccdeb463b2453f8c4e250576
kamir222 <katia.amir@gmail.com>
2017-06-01 21:47:52 +0000

    creating starter code myMap function returining an empty array

--- map/mymap.js
+++ map/mymap.js
@@ -2,6 +2,11 @@ var words = ["ground", "control", "to", "major", "tom"];
 
 //my map function
 
+function myMap (array, function) {
+	newArray = [];
+	return newArray;
+}
+
 map(words, function(word) {
   return word.length;
 });
