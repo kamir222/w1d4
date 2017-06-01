77738ff026de7d9b4abcd4af28eff82fd4e30cd5
kamir222 <katia.amir@gmail.com>
2017-06-01 21:54:34 +0000

    working map function

--- map/mymap.js
+++ map/mymap.js
@@ -2,11 +2,16 @@ var words = ["ground", "control", "to", "major", "tom"];
 
 //my map function
 
-function myMap (array, function) {
+function myMap (array, func) {
 	newArray = [];
+	array.forEach(function(value, index, arr) {
+		newArray.push(func(value));
+	});
 	return newArray;
 }
 
-map(words, function(word) {
+var newMap = myMap(words, function(word) {
   return word.length;
 });
+
+console.log(newMap);
